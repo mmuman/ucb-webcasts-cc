0823ce729a56311d9f2684b4a21610c4c091ad83
François Revol <revol@free.fr>
2017-03-12 02:40:19 +0100

    First attempt at generating master pad content
    An index of all videos and pads, as markdown. Not sure all etherpads
    import markdown properly...

---
+++ generate_master_pad.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+#
+# Copyright 2017, François Revol <revol@free.fr>
+#
+
+import sys
+import urllib.request
+import json
+import youtube_dl
+
+pad_base = '' # etherpad base URL
+
+md = ''
+i = 0
+
+with open('ucb_webcasts.json', 'r') as f:
+    data = json.load(f)
+
+    for course in data:
+        md += "# %s\n" % course['title']
+        md += "Department: %s\n" % course['dept']
+        md += "Semester: %s\n" % course['semester']
+        md += "Lecturer: %s\n" % course['lecturer']
+        md += course['descr']
+        md += '\n'
+
+
+        #print(course)
+        if not 'playlist' in course:
+            md += '*(no playlist)*\n'
+        else:
+            md += 'Playlist: https://www.youtube.com/view_play_list?p=' + course['youTube'] + '\n\n'
+            for item in course['playlist']:
+                md += "## %s\n" % item['title']
+                md += 'URL: https://www.youtube.com/watch?v=%s\n' % item['url']
+                md += 'Pad: %s/UCB_Webcasts_CC_%s\n' % (pad_base, item['url'])
+                md += 'Done: 0%\n'
+                md += '\n'
+
+        md += '\n\n'
+
+        # XXX: testing...
+        i+=1
+        #if i == 5:
+        #    break;
+
+
+    #print(md)
+
+with open('master_pad.md', 'w') as o:
+    o.write(md)
