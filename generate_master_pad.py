57195fb8bfc5172670305f74858d6c49a06d4c51
François Revol <revol@free.fr>
2017-03-12 04:11:20 +0100

    Add newlines
    to force paragraphs, and newlines in html conversion with pandoc

--- generate_master_pad.py
+++ generate_master_pad.py
@@ -19,23 +19,23 @@ with open('ucb_webcasts.json', 'r') as f:
 
     for course in data:
         md += "# %s\n" % course['title']
-        md += "Department: %s\n" % course['dept']
-        md += "Semester: %s\n" % course['semester']
-        md += "Lecturer: %s\n" % course['lecturer']
+        md += "Department: %s\n\n" % course['dept']
+        md += "Semester: %s\n\n" % course['semester']
+        md += "Lecturer: %s\n\n" % course['lecturer']
         md += course['descr']
-        md += '\n'
+        md += '\n\n'
 
 
         #print(course)
         if not 'playlist' in course:
-            md += '*(no playlist)*\n'
+            md += '*(no playlist)*\n\n'
         else:
             md += 'Playlist: https://www.youtube.com/view_play_list?p=' + course['youTube'] + '\n\n'
             for item in course['playlist']:
                 md += "## %s\n" % item['title']
-                md += 'URL: https://www.youtube.com/watch?v=%s\n' % item['url']
-                md += 'Pad: %s/UCB_Webcasts_CC_%s\n' % (pad_base, item['url'])
-                md += 'Done: 0%\n'
+                md += 'URL: https://www.youtube.com/watch?v=%s\n\n' % item['url']
+                md += 'Pad: %s/UCB_Webcasts_CC_%s\n\n' % (pad_base, item['url'])
+                md += 'Done: 0%\n\n'
                 md += '\n'
 
         md += '\n\n'
