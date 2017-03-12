fa7c4a955062cdaa6fd714a1a26e714e7f1c3df3
François Revol <revol@free.fr>
2017-03-12 05:35:21 +0100

    Split pads by semester

--- generate_master_pad.py
+++ generate_master_pad.py
@@ -4,21 +4,34 @@
 # Copyright 2017, François Revol <revol@free.fr>
 #
 
+import collections
 import sys
 import urllib.request
 import json
 import youtube_dl
 
-pad_base = '' # etherpad base URL
+pad_base = 'https://etherpad.wikimedia.org/p' # etherpad base URL
 
 md = ''
 i = 0
+
+semesters = collections.OrderedDict()
+semester = None
+
 
 with open('ucb_webcasts.json', 'r') as f:
     data = json.load(f)
 
     for course in data:
-        md += "# %s\n" % course['title']
+
+        sem = course['semester'].replace(' ', '_')
+        if sem not in semesters:
+            semesters[sem] = ''
+            md = semesters[sem]
+            semester = sem
+
+        #md += "# %s\n" % course['title']
+        md += "**%s**\n\n" % course['title']
         md += "Department: %s\n\n" % course['dept']
         md += "Semester: %s\n\n" % course['semester']
         md += "Lecturer: %s\n\n" % course['lecturer']
@@ -32,21 +45,31 @@ with open('ucb_webcasts.json', 'r') as f:
         else:
             md += 'Playlist: https://www.youtube.com/view_play_list?p=' + course['youTube'] + '\n\n'
             for item in course['playlist']:
-                md += "## %s\n" % item['title']
+                #md += "## %s\n" % item['title']
+                md += "**_%s_**\n\n" % item['title']
                 md += 'URL: https://www.youtube.com/watch?v=%s\n\n' % item['url']
                 md += 'Pad: %s/UCB_Webcasts_CC_%s\n\n' % (pad_base, item['url'])
                 md += 'Done: 0%\n\n'
+                md += 'Takers: \n\n'
                 md += '\n'
 
+        md += '\n\n'
         md += '\n\n'
 
         # XXX: testing...
         i+=1
         #if i == 5:
         #    break;
-
+        semesters[semester] = md
 
     #print(md)
 
+master = '# Semester pads:\n'
+for s in semesters:
+    md = semesters[s]
+    with open('pad_'+s+'.md', 'w') as o:
+        o.write(md)
+    master += 'Pad: %s/UCB_Webcasts_CC_%s\n\n' % (pad_base, s)
+
 with open('master_pad.md', 'w') as o:
-    o.write(md)
+    o.write(master)
